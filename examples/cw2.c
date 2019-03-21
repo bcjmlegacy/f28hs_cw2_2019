cd83af6ba0e95f6c9e0467f3a8602fd56afb64d1
BCJMilne <ben.cj.milne@gmail.com>
2019-03-21 15:42:08 +0000

    1553182522
    Signed-off-by: BCJMilne <ben.cj.milne@gmail.com>

---
+++ examples/cw2.c
@@ -0,0 +1,25 @@
+/*
+    cw2.c
+    F28HS Coursework 2
+
+    Author:         Benjamin Milne & Alakbar Zeynalzade
+    Inst.:          Heriot-Watt University
+    ID:             H00267054 & HXXXXXXXX
+    Contact:        bm56@hw.ac.uk & az40@hw.ac.uk
+    Date:           March, 2019
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef int bool;
+#define TRUE    1
+#define FALSE   0
+
+int main(int argc, char *argv[])
+{
+    printf("%s\nF28HS Coursework 2\n\n", argv[0]);
+
+
+}
---
+++ examples/makefile
@@ -0,0 +1,2 @@
+cw2make: cw2.c
+	gcc cw2.c -o cw2
