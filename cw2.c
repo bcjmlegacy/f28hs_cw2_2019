85e66cab419be0ae8a1d6781a08a601c96889561
Sam <sam@t450.lan>
2019-03-21 15:10:22 +0000

    inital commit

---
+++ cw2.c
@@ -0,0 +1,19 @@
+/*
+    cw2.c
+    F28HS Coursework 2
+
+    Author:         Benjamin Milne & Alakbar Zeynalzade
+    Inst.:          Heriot-Watt University
+    ID:             H00267054 & 
+    Contact:        bm56@hw.ac.uk & az40@hw.ac.uk
+    Date:           March, 2019
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    printf("What's up");
+}
