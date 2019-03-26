4fd61105ef9a727d69735ba7fb9dc7eb9d569dd1
Melanthios <alakbar234@gmail.com>
2019-03-26 18:25:05 +0000

    Bug fixes and performance improvements

--- cw2.alakbar.c
+++ cw2.alakbar.c
@@ -9,29 +9,23 @@
     Date:       March, 2019
 */
 
-
-
-    #define _XOPEN_SOURCE
-    #include <unistd.h>
-
-
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include <poll.h>
+// #include <poll.h>
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
 #include <time.h>
 #include <fcntl.h>
-#include <pthread.h>
+// #include <pthread.h>
 #include <sys/time.h>
-#include <sys/mman.h>
+// #include <sys/mman.h>
 #include <sys/stat.h>
-#include <sys/wait.h>
-#include <sys/ioctl.h>
+// #include <sys/wait.h>
+// #include <sys/ioctl.h>
 
 #ifndef TRUE
     typedef int bool;
@@ -174,6 +168,7 @@ int main(int argc, char *argv[])
 
 
     //
+    CE();
     printf("[PLAYER TWO]");
 
 
